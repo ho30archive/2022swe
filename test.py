b334072d31119db2cd895c39fa5a5f403a3b4276
ho30archive <ho30archive@gmail.com>
2022-11-24 14:47:38 +0900

    commit4

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def test(a, b);
     if a == b:
         logging.debug('Test succeeded')
@@ -5,3 +6,12 @@ def test(a, b);
     else:
         logging.info('Test failure')
         print('False')
+=======
+def test(a, b):
+	if a != b:
+		logging.info('Test succeeded')
+		print("True")
+	else:
+		logging.info('Test failure')
+		print("False")
+>>>>>>> 731678e (commit4)
