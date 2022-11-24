d7a4bf89f76c0a9d6d76059e83e48c0a58b3ab9f
ho30archive <ho30archive@gmail.com>
2022-11-24 16:27:47 +0900

    Change log level for the success case

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def test(a, b);
     if a == b:
-        logging.info('Test succeeded')
+        logging.debug('Test succeeded')
         print('True')
     else:
         logging.info('Test failure')
